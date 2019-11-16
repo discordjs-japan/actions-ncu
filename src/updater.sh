a4ea252e43b82d638e648f0a111985b9c5d00f51
InkoHX <inkohx@gmail.com>
2019-11-16 23:16:19 +0900

    Add updater script.

---
+++ src/updater.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "[INFO] Install npm-check-updates"
+
+npm i -g npm-check-updates
+
+cd $GITHUB_WORKSPACE
+
+echo "[INFO] Setting git config."
+
+REMOTE="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git config user.name "DependenciesUpdater"
+git config user.email "InkoHX@users.noreply.github.com"
+git remote set-url origin $REMOTE
+
+echo "Check for updates."
+
+ncu -u --packageFile package.json
+
+if [[ `git status --porcelain` ]]; then
+  echo "Update found!"
+  echo "Updating lock file..."
+  npm install
+
+  echo "Commit and Push"
+  git add .
+  git commit -m "Updated all dependencies"
+  git push origin HEAD:master
+fi
